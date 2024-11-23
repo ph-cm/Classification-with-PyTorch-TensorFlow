dd04c45108b87a08bc0011b090cb59159d54cc35
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2024-11-22 21:27:56 -0300

    Split the Data into Train and Test

--- classification.py
+++ classification.py
@@ -49,3 +49,26 @@ plt.show()
 # Pairplot for L2 normalized data
 sns.pairplot(df_l2, hue='Label')
 plt.show()
+
+#Split the Data into Train and Test
+#Split the data into train and test sets(80%train, 20%test)
+X_train, X_test, Y_train, Y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+
+#Normalize the train and test sets using L2 normalization
+X_train_normalized = normalize(X_train, norm="l2", axis=1)
+X_test_normalized = normalize(X_test, norm="l2", axis=1)
+
+#Convert to DataFrames for visualization
+df_train = pd.DataFrame(X_train_normalized, columns=feature_names).assign(Label=Y_train)
+df_test = pd.DataFrame(X_test_normalized, columns=feature_names).assign(Label=Y_test)
+
+#Visualize the trainning data
+sns.pairplot(df_train, hue='Label')
+plt.title('L2 Normalized Training Data')
+plt.show()
+
+#Visualize the testing data
+sns.pairplot(df_test, hue='Label')
+plt.title('L2 Normalized Test Data')
+plt.show()
+
